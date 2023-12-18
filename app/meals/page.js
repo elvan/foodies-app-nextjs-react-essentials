ee59b8c921f4fc6b69f8691706350fa62d38e57b
Elvan Hidayat <elvanhd@gmail.com>
2023-12-18 23:24:09 +0700

    adding static and dynamic metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -5,7 +5,13 @@ import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 import classes from './page.module.css';
 
+export const metadata = {
+  title: 'All Meals',
+  description: 'Browse the delicious meals shared by our vibrant community.',
+};
+
 async function Meals() {
+  console.log('Fetching meals');
   const meals = await getMeals();
 
   return <MealsGrid meals={meals} />;
